42cc3c658207440f36d364b74a4e9e2886c93f4f
vlzx <lzxbp811@gmail.com>
2021-02-25 15:37:24 +0800

    Update Demo

--- great_module.pyx
+++ great_module.pyx
@@ -1,6 +1,8 @@
+import re
+
 def great_function(text):
     print('Great!')
     text = text.decode('utf-8')
-    text = f'[ {text} ]'
+    text = re.sub(r'(\d+)', r'[\g<1>]', text)
     text = text.encode('utf-8')
     return text
--- main.cpp
+++ main.cpp
@@ -9,7 +9,7 @@ int main(int argc, char *argv[]) {
     Py_Initialize();
     PyImport_ImportModule("caller");
 
-    std::cout << call_great_function("Hello 你好") << std::endl;
+    std::cout << call_great_function("Hello 你好 1") << std::endl;
 
     Py_Finalize();
     return 0;
